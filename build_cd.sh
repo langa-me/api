7a255fd167e09bb530afc1db071f6623cc0d0a1c
Louis Beaumont <louis.beaumont@gmail.com>
2022-01-25 20:35:05 +0000

    1.0.3: fix broken cd

--- build_cd.sh
+++ build_cd.sh
@@ -1,10 +1,10 @@
 
 #!/bin/bash
 
-echo "Getting started"
+echo "Building redoc..."
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi/openapi.yaml && \
+npx redoc-cli@0.13.2 bundle openapi/openapi.yaml && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
